c18a8a04d7e63fa15183018250ff2a437863bf54
jfss1 <jose.filipe.sampaio@live.com.pt>
2021-06-06 16:22:10 +0100

    4th commit

--- src/app/mapa/mapa.page.ts
+++ src/app/mapa/mapa.page.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import {ViewChild, ElementRef} from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.page.html',
   styleUrls: ['./mapa.page.scss'],
 })
-export class MapaPage implements OnInit {
+export class MapaPage{
+
+  map: any;
+
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
 
   constructor() { }
 
-  ngOnInit() {
+  ionViewDidEnter(){
+    this.showMap();
+  }
+
+  showMap(){
+    const location = new google.maps.LatLng(-17.824858, 31.053028);
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
   }
 
 }
